86811e1f8e02d25cf988e546cc417d96dd0aaa15
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-11-17 23:02:17 -0500

    Require tables

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 const logo = require("asciiart-logo");
 const config = require("./package.json");
 
