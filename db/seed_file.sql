67f3b0b74c173186786206e4c6fe1f24eadef4cb
jrtwheeler <58869889+jrtwheeler@users.noreply.github.com>
2020-12-06 22:38:07 -0500

    Revised seed file

--- db/seed_file.sql
+++ db/seed_file.sql
@@ -2,4 +2,4 @@ INSERT INTO department (name) VALUES ('sales'), ('accounting');
 
 INSERT INTO role (title, salary, department_id) VALUES ('front-end', 75000, 1), ('back-end', 70000, 2);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Maddy', "Wheeler", 1, null), ('James', 'Liem', 2, 1);
+INSERT INTO employee (first_name, last_name) VALUES ('Maddy', "Wheeler"), ('James', 'Liem');
